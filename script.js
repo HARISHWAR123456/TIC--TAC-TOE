820329d6bfa84d39a97ca0019ec3d4bcbc0402f5
HARISHWAR123456 <138799197+HARISHWAR123456@users.noreply.github.com>
2024-07-12 12:52:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let boxes = document.querySelectorAll(".box")
+let resetBtn = document.querySelector("#reset-btn"); 
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+
+let turnO = true;
+let count = 0;
+
+let winPatten = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        
+      if(turnO){
+        box.innerHTML = "O";
+        console.log(box);
+        turnO = false;
+
+      }else{
+        box.innerHTML = "X";
+        turnO = true;
+
+      }
+
+      box.disable = "true";
+      count++;
+
+      checkWinner();
+
+    });
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation ,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    
+
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatten) {
+        let pos1val = boxes [pattern [0]].innerHTML;
+        let pos2val = boxes [pattern [1]].innerHTML;
+        let pos3val = boxes [pattern [2]].innerHTML;
+
+
+        if(pos1val != "" && pos2val != "" && pos3val != ""){
+            if (pos1val === pos1val && pos2val === pos3val){
+                // alert("Player" + pos1val + " won the game");
+                showWinner(pos1val);
+                return true;
+            }
+        }
+    }
+
+
+    };
